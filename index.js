2badb8e4ee797aece7198cf97f61c0c1415d8ae0
jevonwill <jevonwilliamsjr@gmail.com>
2022-12-15 11:03:00 -0500

    got form data into object and operated on it

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const calcTotal = document.getElementById("calc"); calcTotal.addEventListener("submit", handleSubmit);
 
+
+//Function is run on submit button click, creates an object out of the form data and logs to console
 function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
-    console.log(formProps);
+    const quote = formProps.width * formProps.length + formProps.value;
+    console.log(quote);
+
   }
 
   
