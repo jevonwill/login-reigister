168abc1259bd0b6a2e83977a17d761f6e43942c2
jevonwill <jevonwilliamsjr@gmail.com>
2022-12-15 11:12:57 -0500

    BASIC CALC WORKS!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const calcTotal = document.getElementById("calc"); calcTotal.addEventListener("submit", handleSubmit);
-
+const results = document.querySelector('.results');
 
 //Function is run on submit button click, creates an object out of the form data and logs to console
 function handleSubmit(e) {
@@ -8,7 +8,7 @@ function handleSubmit(e) {
     const formProps = Object.fromEntries(formData);
     const quote = formProps.width * formProps.length + formProps.value;
     console.log(quote);
-
+    results.innerHTML = `<h1>${quote}</h1>`
   }
 
   
